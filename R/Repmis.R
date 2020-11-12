3ea469870a384eeddec75be9f084f8f80e3c44b2
Andy Tai <andymytai@alumni.ubc.ca>
2020-11-12 11:23:36 -0800

    Edits

---
+++ R/Repmis.R
@@ -0,0 +1,10 @@
+#'
+#'
+#'
+
+Repmis <- function(x, fun = median, verbose = FALSE) {
+  if(!is.numeric(x)) {
+    message('Error, this funcion only works to replace missing values in numeric vectors. Instead we have replaced missing values with "none"','Looks you put in class: ', class(x)[1], replace(x, is.na(x), "none")) }
+  if(verbose) {message("replacing missing")}
+  replace(x, is.na(x), simple.impute(x, fun = fun))
+}
